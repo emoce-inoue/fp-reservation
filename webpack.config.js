24702f9c3ad01d89f445c46f479341a92986dd53
inoue <emocedigital@gmail.com>
2025-01-06 16:15:29 +0900

    webpackの設定変更

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,58 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const glob = require('glob');
+
+// HTMLファイルを動的に取得し、必要なバンドルを割り当てる
+const htmlPlugins = glob.sync('./src/**/*.html').map((file) => {
+  const filename = path.relative('./src', file);
+
+  let outputFilename;
+  let chunk;
+
+  if (filename === 'man/index.html') {
+    outputFilename = 'man/index.html';
+    chunk = ['man'];
+  } else if (filename === 'woman/index.html') {
+    outputFilename = 'woman/index.html';
+    chunk = ['woman'];
+  }
+
+  return new HtmlWebpackPlugin({
+    filename: outputFilename,
+    template: file,
+    inject: 'body',
+    chunks: chunk,
+    scriptLoading: 'module',
+    minify: false,
+  });
+});
 
 module.exports = {
   mode: 'production',
-  entry: './src/js/index.js',
+  entry: {
+    man: ['./src/js/utils.js', './src/js/common.js', './src/css/man.css'],
+    woman: ['./src/js/utils.js', './src/js/common.js', './src/css/woman.css'],
+  },
   output: {
-    filename: 'bundle.js',
+    filename: (pathData) => {
+      if (pathData.chunk.name === 'man') {
+        return 'man/js/[name].bundle.js';
+      } else if (pathData.chunk.name === 'woman') {
+        return 'woman/js/[name].bundle.js';
+      }
+    },
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   devServer: {
     static: {
-      directory: path.resolve(__dirname, 'dist'),
+      directory: path.resolve(__dirname, 'src'),
     },
     hot: true,
     open: true,
     watchFiles: {
-      paths: ['src/index.html'],
+      paths: ['src/**/*.html', 'src/**/*.css', 'src/**/*.js'],
       options: {
         usePolling: true,
       },
@@ -50,17 +85,19 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: ['.js'], // ES6モジュールを認識する
+    extensions: ['.js'],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'style.css',
+      filename: ({ chunk }) => {
+        if (chunk.name === 'man') {
+          return 'man/css/[name].css';
+        } else if (chunk.name === 'woman') {
+          return 'woman/css/[name].css';
+        }
+      },
     }),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      inject: 'body',
-      minify: false,
-    }),
+    ...htmlPlugins,
     new CopyWebpackPlugin({
       patterns: [{ from: 'src/images', to: 'images' }],
     }),
